0751da632b102665ae73466e68346c9a1824138d
Diogo-Paulico <55745304+Diogo-Paulico@users.noreply.github.com>
2020-03-13 13:44:33 +0000

    Script v0.6Beta

--- bash.sh
+++ bash.sh
@@ -1,8 +1,9 @@
-
 echo "If you have run this setup before and the computer was reset press 2 and enter, otherwise press 1 and enter"
 select yn in "FirstInstall" "ContinueInstall"; do
     case $yn in
         FirstInstall )
+        apt update
+        apt upgrade -y
         if [[ $(which java) ]]; then
     echo "Java is installed"
 else
@@ -18,25 +19,24 @@ select yn in "Yes" "No"; do
     esac
 done 
 clear
- apt upgrade -y
  apt -qq install libpam0g:i386 libx11-6:i386 libstdc++6:i386 libstdc++5:i386 libnss3-tools xterm openssl -y
  mkdir vpnInstaller
  cd ./vpnInstaller
- wget -q https://vpn.fct.unl.pt/sslvpn/SNX/INSTALL/cshell_install.sh --no-check-certificate
+  read -p "Refer to the instruction guide (step2). After accepting security exception press Enter"
  wget -q https://vpn.fct.unl.pt/sslvpn/SNX/INSTALL/snx_install.sh --no-check-certificate
+ bash ./snx_install.sh;
  read -p "After rebooting the computer, reopen the setup file and select number 2"
  reboot -f
         break;;
 ContinueInstall ) 
         cd ./vpnInstaller
-         bash ./snx_install.sh;
+        echo "Refer to the instruction guide (step3), after doing the steps there mentioned, press Enter to continue"
         bash ./cshell_install.sh;
-        sleep 3;
         cd ..;
         rm -r vpnInstaller;
         clear;
-        read -p "After rebooting the computer, follow the remaining steps to finish the setup. Press enter to reboot your computer.";
-        shutdown -r now; 
+        read -p "After rebooting the computer, you can now use the vpn by acessing it's website. Press enter to reboot your computer.";
+        reboot -f; 
          break;;
     esac
 done 
