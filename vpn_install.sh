0eed51be238565c5e1096dd68c5b2feea2203cb7
João Arvana <joaomanuel.arvana@gmail.com>
2020-03-16 05:21:13 +0000

    Removed the need for a restart making it easier. Added verbosity.
    Some steps have been removed because they are unnecessary, for example the restart. These changes make the install process a lot less involved for the user thus making it a lot easier. The only user interaction needed now is to open and close Firefox before the cshell install, and for them to accept both certificates ( https://localhost:14186/id/  and https://vpn.fct.unl.pt/) and connect.
    Commented out the upgrade step because I feel like it's unnecessary, and a user might want some old version of something installed don their computer so we shouldn't update if for them.
    Added verbosity to the script so the user knows whats going on while the script runs.

--- vpn_install.sh
+++ vpn_install.sh
@@ -1,56 +1,65 @@
 #!/bin/bash
 
 clear
-echo "If you have run this setup before and the computer was reset press 2 and enter, otherwise press 1 and enter"
-select yn in "FirstInstall" "ContinueInstall"; do
-    case $yn in
-    FirstInstall)
-        apt update
-        apt upgrade -y
-        if [[ $(which java) ]]; then
-            echo "Java is installed"
-        else
-            apt -qq update
-            apt -qq install default-jre -y
-        fi
-        clear
-        echo "Do you have Firefox installed? Select number and press Enter"
-        select yn in "Yes" "No"; do
-            case $yn in
-            Yes) break ;;
-            No)
-                apt -qq update
-                apt -qq install firefox -y
-                break
-                ;;
-            esac
-        done
-        clear
-        apt -qq install libpam0g:i386 libx11-6:i386 libstdc++6:i386 libstdc++5:i386 libnss3-tools xterm openssl -y
-        mkdir vpnInstaller
-        cd ./vpnInstaller
-        clear
-        read -p "Refer to the instruction guide (step2). After accepting security exception press Enter"
-        wget -q https://vpn.fct.unl.pt/sslvpn/SNX/INSTALL/snx_install.sh --no-check-certificate
-        bash ./snx_install.sh
-        clear
-        read -p "After rebooting the computer, reopen the setup file and select number 2. Press enter to reboot."
-        reboot -f
-        break
-        ;;
-    ContinueInstall)
-        cd ./vpnInstaller
-        clear
-        read -p "Refer to the instruction guide (step3), after doing the steps there mentioned, press Enter to continue"
-        wget -q https://vpn.fct.unl.pt/sslvpn/SNX/INSTALL/cshell_install.sh --no-check-certificate
-        bash ./cshell_install.sh
-        sleep 3
-        cd ..
-        rm -r vpnInstaller
-        clear
-        read -p "After rebooting the computer, you can now use the vpn by acessing it's website. Press enter to reboot your computer."
-        reboot -f
-        break
-        ;;
-    esac
-done
+read -p "This script checks for java and firefox and installs them if they're not installed already, then it will install the dependencies needed, and then the vpn programs. Follow the on-screen instructions and go back to the guide when told to. Press Enter to start the installation."
+
+apt -qq update
+# apt upgrade -y
+# while updating is necessary to be sure we are downloading the most recent versions of the dependencies,
+# I don't think we should upgrade the already installed software on the users computer (except for the stuff we are going to use ofc)
+# because they migth want to use an older version of something
+
+echo""
+if [[ $(which java) ]]; then
+    echo "1/6 - Java is installed"
+else
+    echo "1/6 - Java not found"
+    echo "1/6 - Installing Java"
+    apt -qq update
+    apt -qq install default-jre -y
+    echo "1/6 - Java has been installed"
+fi
+
+echo""
+if [[ $(which firefox) ]]; then
+    echo "2/6 - Firefox is installed"
+else
+    echo "2/6 - Firefox not found"
+    echo "2/6 - Installing Firefox"
+    apt -qq update
+    apt -qq install firefox -y
+    echo "2/6 - Firefox has been installed"
+fi
+
+echo""
+echo "3/6 - Installing dependencies"
+apt -qq install libpam0g:i386 libx11-6:i386 libstdc++6:i386 libstdc++5:i386 libnss3-tools xterm openssl -y
+echo "3/6 - Dependencies have been installed"
+
+mkdir vpnInstaller
+cd ./vpnInstaller
+
+echo""
+echo "4/6 - Installing snx"
+wget -q -O snx_install.sh https://vpn.fct.unl.pt/sslvpn/SNX/INSTALL/snx_install.sh --no-check-certificate
+chmod +x ./snx_install.sh
+./snx_install.sh
+echo "4/6 - snx installed"
+
+echo""
+read -p "Firefox needs to be opened as part of the setup (if you never opened it before), please open and close it and then press Enter to continue."
+
+echo""
+echo "5/6 - Installing cshell"
+wget -q -O cshell_install.sh https://vpn.fct.unl.pt/sslvpn/SNX/INSTALL/cshell_install.sh --no-check-certificate
+chmod +x ./cshell_install.sh
+./cshell_install.sh
+echo "5/6 - cshell installed"
+
+echo""
+echo "6/6 - Starting cleanup"
+cd ..
+rm -r vpnInstaller
+echo "6/6 - Cleanup complete"
+
+echo "Now go back to the guide. You may also delete this script, and uninstall firefox if it was installed during this setup and you have no use for it."
